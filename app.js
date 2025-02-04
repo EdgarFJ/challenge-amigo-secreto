e4d4f8dcadd0dbe03e33516dbc894f14d04f9f9a
EdgarFJ <edjufelipe@gmail.com>
2025-02-03 22:53:24 -0600

    Se crea funcion para agregar nombre al array

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Declarar un array para almacenar los nombres de los amigos
+let amigos = [];
+
+// Declarar una función para agregar un amigo al array
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    
+    if (nombre && !amigos.includes(nombre)) {
+        amigos.push(nombre);
+        console.log(`${nombre} ha sido agregado a la lista.`);
+        input.value = "";
+    } else {
+        console.log("El nombre está vacío o ya está en la lista.");
+    }
+}
